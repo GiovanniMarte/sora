29b385fc12c47bd875d71eb48cd88b9fe1bb34a7
GiovanniMarte <alumno.98452@ies-azarquiel.es>
2022-09-13 18:17:04 +0200

    build(packages/client): Remplazado chakra-ui con tailwindcss + daisyui

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 if (import.meta.hot) {
   import.meta.hot.on('vite:beforeUpdate', () => console.clear());
