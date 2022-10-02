f09dd214ea0889e9f6060d2901e31b5d01215268
GiovanniMarte <alumno.98452@ies-azarquiel.es>
2022-10-02 17:01:08 +0200

    chore(packages/server): Setup para usar distintos .env

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -4,9 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
